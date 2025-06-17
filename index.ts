5c6dc56d5e5d47195448e0cf191802a4cc35c81f
Weber-droid <emmanueltotimeh2002@gmail.com>
2025-06-17 15:18:05 +0000

    Fixxx

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-console.log("Welcome to my typescript journey!!!!!!!!!")
+import express from "express"
+
+const app = express()
+
+app.use(express.json())
+
+
+
+const PORT = process.env.PORT || 3000
+app.listen(
+    PORT, () => {
+        console.log(`Server is running on localhost ${PORT}.`)
+    }
+)
