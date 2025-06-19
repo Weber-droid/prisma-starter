041ebf2c9fea6185f5404504c18f33e1edee1247
Weber-droid <emmanueltotimeh2002@gmail.com>
2025-06-19 01:35:46 +0000

    fix

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ app.use(express.json())
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
-    console.log(`Server is running on Port ${PORT}`)
+    console.log(`Server is running on localhost ${PORT}`)
 })
